cb6f51da765ecc40737959ee611c43d1dd5a22ef
Jamie Bertram <jamie@genm.co>
2019-10-16 17:43:06 -0400

    feat(analyze): add analyze CLI option
    Added new option, analyze to CLI
    
    BREAKING CHANGE: Testing major versions

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,51 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE RecordWildCards    #-}
+
 module Main where
 
+import           Data.List
+import           Data.Maybe
+import           Git
+import           System.Console.CmdArgs
+import           Types
+
+data Versiontool
+  = Analyze
+  | Changelog
+  deriving (Show, Data, Typeable)
+
+analyze = Analyze
+
+changelog = Changelog
+
 main :: IO ()
-main = pure ()
+main = handle =<< cmdArgs (modes [analyze, changelog])
+
+handle :: Versiontool -> IO ()
+handle Analyze =
+  maybe
+    (pure ())
+    (\Version {..} ->
+       putStrLn $
+       show _versionMajor ++
+       "." ++ show _versionMinor ++ "." ++ show _versionPatch) =<<
+  handleAnalyze (Version 0 0 0) <$> getCommitsSinceLastRelease
+handle Changelog = error "Not implemented"
+
+handleAnalyze :: Version -> [Commit] -> Maybe Version
+handleAnalyze Version {..} commits
+  | any hasBreakingChange commits = Just $ Version (_versionMajor + 1) 0 0
+  | any hasFeatureChange commits =
+    Just $ Version _versionMajor (_versionMinor + 1) 0
+  | any hasFixChange commits =
+    Just $ Version _versionMajor _versionMinor (_versionPatch + 1)
+  | otherwise = Nothing
+
+hasBreakingChange :: Commit -> Bool
+hasBreakingChange = isInfixOf "BREAKING CHANGE" . _commitBody
+
+hasFeatureChange :: Commit -> Bool
+hasFeatureChange = (== Feat) . _commitType
+
+hasFixChange :: Commit -> Bool
+hasFixChange = (== Fix) . _commitType
